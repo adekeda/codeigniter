4956bfdf11c905b0e0e5f713cdb1f12d22db01f4
adekeda <adek.muhammad.abdullah@gmail.com>
2020-05-04 06:13:58 +0800

    route contact dan about .php

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+//tambah route baru untuk contact dan about us
+$route['about'] = 'welcome/about';
+$route['contact'] = 'Welcome/contact';
